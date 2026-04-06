e1d1787673fc3b258b61e057d6985ff0b9e4be42
soqu4c <soqu4c@gmail.com>
2026-04-06 21:40:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -301,3 +301,13 @@ window.addEventListener('load', () => {
         }
     }, 1000);
 });
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Запрет некоторых комбинаций клавиш (F12, Ctrl+Shift+I)
+document.onkeydown = function(e) {
+    if(e.keyCode == 123) return false; // F12
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) return false;
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
+};
